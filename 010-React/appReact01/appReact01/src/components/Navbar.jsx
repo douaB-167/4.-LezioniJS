4ea2ffe7ed87ef11de05fafe3934b2f3417df432
douaB-167 <doua.boufqir@edu.immaginazioneelavoro.it>
2025-07-17 14:23:46 +0200

    Add files via upload

---
+++ 010-React/appReact01/appReact01/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <>
+            <ul>
+                <li>Pag 1</li>
+                <li>Pag 2</li>
+                <li>Pag 3</li>
+                <li>Pag 4</li>
+            </ul>
+        </>
+    )
+}
+
+export default Navbar
---
+++ 010-React/appReact01/appReact02/components/Card.jsx
@@ -0,0 +1,102 @@
+import './Card.css';
+
+// function Card(){
+
+//     const title = "Tucano";
+//     const imgURL = "https://images.unsplash.com/photo-1618191702724-1e413e177fde?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+//     const description = "Questo è un Tucano"
+
+//     return(
+//         <div className="card">
+//             <div className="card-image">
+//                 <img src={imgURL} alt="" />
+//             </div>
+//             <div>
+//                 <h2>{title}</h2>
+//                 <p>{description}</p>
+//             </div>
+//         </div>
+//     )
+
+// }
+
+//ATT: in questo modo io definisco delle props le quali verranno valorizzate nel component
+//Parent. Il dato esiste nel parent
+// function Card(props){
+//     const title = props.title;
+//     const description = props.description;
+//     const imgURL = props.imgURL;
+
+//     return(
+//         <div className="card">
+//              <div className="card-image">
+//                  <img src={imgURL} alt="" />
+//              </div>
+//              <div>
+//                  <h2>{title}</h2>
+//                  <p>{description}</p>
+//              </div>
+//          </div>
+//     )
+// }
+
+//in questa "versione" non passo un parametro chiamato props ma passo direttamente un oggetto
+// function Card({title, description, imgURL}){
+//     return(
+//         <div className="card">
+//               <div className="card-image">
+//                   <img src={imgURL} alt="" />
+//               </div>
+//               <div>
+//                   <h2>{title}</h2>
+//                   <p>{description}</p>
+//               </div>
+//           </div>
+//     )
+// }
+
+//in questa "versione" utilizzerò una parola chiave: "children" per poter raccogliere 
+//ciò che scrivo a mano nel parent all'interno del selettore child
+//Quindi children non è una semplice prop, bensì una parola chiave
+
+function Event(){
+    let btnA = document.querySelector("#btnA");
+    let btnNA = document.querySelector("#btnA");
+
+    btnA.addEventListener("onclick", function(){
+        // if(!isSpotted){
+        //     isSpotted.innerHTML += "Non avvistato";
+
+        // }
+    })
+
+    btnNA.addEventListener("onclick", function(){
+
+    })
+}
+
+function Card({ title, description, imgURL, children, isSpotteed }) {
+    return (
+        <div className="card">
+            <div className={`${isSpotteed ? "BgGreen" : "BgRed"}`}>
+                <div className="card-image">
+                    <img src={imgURL} alt="" />
+                </div>
+                <div>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <p>Commento: {children}</p>
+                </div>
+                <div>
+                    <span>
+                        {isSpotteed ? "Avvistato" : "Non avvistato"}
+                    </span>
+                </div>
+                <button id='btnA'>Avvistato</button>
+                <button id='btnNA'>Non Avvistato</button>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
